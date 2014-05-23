0dcacd925ee16d6e692eb295ecff633f7481be3a
Ruben Nieves <rubennm@gmail.com>
2014-05-23 08:33:14 -0500

    1) Readme update 2) Added main EasyPost file

---
+++ EasyPostSample/Easypost/EasyPost.m
@@ -0,0 +1,227 @@
+//
+//  EasyPost.m
+//  EasyPostSample
+//
+//  Created by Ruben Nieves on 5/21/14.
+//  Copyright (c) 2014 TopBalance Software. All rights reserved.
+//
+
+#import "EasyPost.h"
+
+#if (TEST_VERSION)
+NSString * const APIKEY                                     = @""; //Test Key
+#else
+NSString * const APIKEY                                     = @"";
+#endif
+NSString * const EasyPostServiceErrorDomain					= @"EasyPostServiceError";
+static char base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+@implementation EasyPost
+
+#pragma mark - Special Methods
++ (NSString *)base64EncodeForData:(NSData *)dataToEncode;
+{
+    NSMutableString *dest = [[NSMutableString alloc] initWithString:@""];
+    unsigned char * working = (unsigned char *)[dataToEncode bytes];
+    int srcLen = [dataToEncode length];
+    for (int i=0; i<srcLen; i += 3) {
+        for (int nib=0; nib<4; nib++) {
+            int byt = (nib == 0)?0:nib-1;
+            int ix = (nib+1)*2;
+            if (i+byt >= srcLen) break;
+            unsigned char curr = ((working[i+byt] << (8-ix)) & 0x3F);
+            if (i+nib < srcLen) curr |= ((working[i+nib] >> ix) & 0x3F);
+            [dest appendFormat:@"%c", base64[curr]];
+        }
+    }
+    return dest;
+}
+
++ (void)requestServiceForParameters:(NSMutableDictionary *)paramsDictionary atLink:(NSString *)link withHandler:(CompletionHandlerBlock)completionHandler
+{
+    NSMutableString *body = [NSMutableString stringWithCapacity:255];
+    [paramsDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [body appendFormat:@"%@=%@&", key, obj];
+    }];
+    NSString *method = @"GET";
+    if ([[paramsDictionary allKeys] count] > 0) {
+        [body deleteCharactersInRange:NSMakeRange([body length]-1, 1)]; //Delete last & character
+        method = @"POST";
+    }
+    
+    
+    NSData *requestData = [body dataUsingEncoding:NSUTF8StringEncoding];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:link]];
+    
+    //Authorization
+    NSString *authStr = [NSString stringWithFormat:@"%@:", APIKEY];
+    NSData *authData = [authStr dataUsingEncoding:NSASCIIStringEncoding];
+    NSString *authValue = [NSString stringWithFormat:@"Basic %@=", [EasyPost base64EncodeForData:authData]];
+    [request setValue:authValue forHTTPHeaderField:@"Authorization"];
+    
+    //Finish request
+    NSString *postLength = [NSString stringWithFormat:@"%d", [requestData length]];
+    [request setHTTPMethod:method];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    NSURLSessionUploadTask *uploadTask =  [session uploadTaskWithRequest:request fromData:requestData  completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)   {
+        //handle response
+        if (error) {
+            completionHandler(error, nil);
+        } else {
+            NSError *jsonError = nil;
+            NSDictionary *returnData =
+            [NSJSONSerialization JSONObjectWithData:data
+                                            options:NSJSONReadingAllowFragments
+                                              error:&jsonError];
+            completionHandler(jsonError, returnData);
+            
+
+        }
+    }];
+    [uploadTask resume];
+    
+}
+
+#pragma mark - Class Methods
++ (void)getPostageLabelForShipment:(NSString *)shipmentId atRate:(NSString *)rateId withCompletionHandler:(CompletionHandlerBlock)completionHandler
+{
+    if (!rateId || !shipmentId) {
+        NSError *error = [NSError errorWithDomain:EasyPostServiceErrorDomain code:EasyPostServiceErrorMissingParameters userInfo:[NSDictionary dictionaryWithObjectsAndKeys:@"Missing required parameters", NSLocalizedDescriptionKey, nil]];
+        completionHandler(error,nil);
+        return;
+    }
+    NSString *link = [NSString stringWithFormat:@"https://api.easypost.com/v2/shipments/%@/buy",shipmentId];
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                 rateId, @"rate[id]",
+                                 nil];
+    
+    [EasyPost requestServiceForParameters:dict atLink:link withHandler:completionHandler];
+}
+
+
+
++ (void)getAddress:(NSMutableDictionary *)addressDict withCompletionHandler:(CompletionHandlerBlock)completionHandler
+{
+    [EasyPost requestServiceForParameters:addressDict atLink:@"https://api.easypost.com/v2/addresses" withHandler:completionHandler];
+}
+
+
++ (void)getShipmentTo:(NSString *)toAddressId from:(NSString *)fromAddressId forParcel:(NSString *)parcelId customsInfo:(NSString *)customsId withCompletionHandler:(CompletionHandlerBlock)completionHandler
+{
+    if (!toAddressId || !fromAddressId || !parcelId) {
+        NSError *error = [NSError errorWithDomain:EasyPostServiceErrorDomain code:EasyPostServiceErrorMissingParameters userInfo:[NSDictionary dictionaryWithObjectsAndKeys:@"Missing required parameters", NSLocalizedDescriptionKey, nil]];
+        completionHandler(error,nil);
+        return;
+    }
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                 toAddressId, @"shipment[to_address][id]",
+                                 fromAddressId, @"shipment[from_address][id]",
+                                 parcelId, @"shipment[parcel][id]",
+                                 nil];
+    if (customsId) {
+        [dict setValue:customsId forKey:@"shipment[customs_info][id]"];
+    }
+    [EasyPost requestServiceForParameters:dict atLink:@"https://api.easypost.com/v2/shipments" withHandler:completionHandler];
+}
+
+
++ (void)getShipmentTo:(NSMutableDictionary *)toAddress from:(NSMutableDictionary *)fromAddress forParcel:(NSMutableDictionary *)parcelDictionary withCompletionHandler:(CompletionHandlerBlock)completionHandler
+{
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    
+    //Add addresses values for shipment
+    for (int i = 0; i < 2; i++) {
+        NSMutableDictionary *addressDict = i == 0 ? fromAddress : toAddress;
+        NSString *addressKeyString = i == 0 ? @"from_address" : @"to_address";
+        
+        NSString *name = [addressDict valueForKey:@"address[name]"];
+        if (name) {
+            [dict setValue:name forKey:[NSString stringWithFormat:@"shipment[%@][name]",addressKeyString]];
+        }
+        
+        NSString *company = [addressDict valueForKey:@"address[company]"];
+        if (company) {
+            [dict setValue:company forKey:[NSString stringWithFormat:@"shipment[%@][company]",addressKeyString]];
+        }
+        
+        NSString *street1 = [addressDict valueForKey:@"address[street1]"];
+        if (street1) {
+            [dict setValue:street1 forKey:[NSString stringWithFormat:@"shipment[%@][street1]",addressKeyString]];
+        }
+        NSString *street2 = [addressDict valueForKey:@"address[street2]"];
+        if (street2) {
+            [dict setValue:street2 forKey:[NSString stringWithFormat:@"shipment[%@][street2]",addressKeyString]];
+        }
+        
+        NSString *city = [addressDict valueForKey:@"address[city]"];
+        if (city) {
+            [dict setValue:city forKey:[NSString stringWithFormat:@"shipment[%@][city]",addressKeyString]];
+        }
+        
+        NSString *state = [addressDict valueForKey:@"address[state]"];
+        if (state) {
+            [dict setValue:state forKey:[NSString stringWithFormat:@"shipment[%@][state]",addressKeyString]];
+        }
+        
+        NSString *zip = [addressDict valueForKey:@"address[zip]"];
+        if (zip) {
+            [dict setValue:zip forKey:[NSString stringWithFormat:@"shipment[%@][zip]",addressKeyString]];
+        }
+        
+        NSString *country = [addressDict valueForKey:@"address[country]"];
+        if (country) {
+            [dict setValue:country forKey:[NSString stringWithFormat:@"shipment[%@][country]",addressKeyString]];
+        } else {
+            [dict setValue:@"US" forKey:[NSString stringWithFormat:@"shipment[%@][country]",addressKeyString]];
+        }
+        
+        NSString *phone = [addressDict valueForKey:@"address[phone]"];
+        if (phone) {
+            [dict setValue:phone forKey:[NSString stringWithFormat:@"shipment[%@][phone]",addressKeyString]];
+        }
+        
+        NSString *email = [addressDict valueForKey:@"address[email]"];
+        if (email) {
+            [dict setValue:email forKey:[NSString stringWithFormat:@"shipment[%@][email]",addressKeyString]];
+        }
+    }
+    
+    
+    NSString *predefinedPackage = [parcelDictionary valueForKey:@"parcel[predefined_package]"];
+    if (predefinedPackage) {
+        [dict setValue:predefinedPackage forKey:@"shipment[parcel][predefined_package]"];
+    }
+    
+    NSString *weight = [parcelDictionary valueForKey:@"parcel[weight]"];
+    if (weight) {
+        [dict setValue:weight forKey:@"shipment[parcel][weight]"];
+    }
+    
+    NSString *length = [parcelDictionary valueForKey:@"parcel[length]"];
+    if (length) {
+        [dict setValue:length forKey:@"shipment[parcel][length]"];
+    }
+    
+    NSString *width = [parcelDictionary valueForKey:@"parcel[width]"];
+    if (width) {
+        [dict setValue:width forKey:@"shipment[parcel][width]"];
+    }
+    
+    NSString *height = [parcelDictionary valueForKey:@"parcel[height]"];
+    if (height) {
+        [dict setValue:height forKey:@"shipment[parcel][height]"];
+    }
+    
+    [EasyPost requestServiceForParameters:dict atLink:@"https://api.easypost.com/v2/shipments" withHandler:completionHandler];
+
+}
+
+
+@end
--- README.md
+++ README.md
@@ -2,3 +2,128 @@ easypost-ios
 ============
 
 Simple implementation of the EasyPost API for iOS. Generates postage easily
+
+
+
+Install
+=================
+
+1. Load all files under EasyPost folder in sample project to your own project.
+2. `#import "EasyPost.h"`
+
+
+Usage
+=================
+
+
+1. Create sender, recipient and parcel dictionaries
+--------------------
+
+```
+	NSMutableDictionary *fromDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                           @"Steve Jobs",@"address[name]",
+                                           @"",@"address[company]",
+                                           @"1 Infinite Loop",@"address[street1]",
+                                           @"",@"address[street2]",
+                                           @"Cupertino",@"address[city]",
+                                           @"CA",@"address[state]",
+                                           @"95014",@"address[zip]",
+                                           @"US",@"address[country]",
+                                           @"(408)974-5050",@"address[phone]",
+                                           @"steve@apple.com",@"address[email]",
+                                           nil];
+    
+    NSMutableDictionary *toDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                         @"Bill Gates",@"address[name]",
+                                         @"",@"address[company]",
+                                         @"1 Microsoft Way",@"address[street1]",
+                                         @"",@"address[street2]",
+                                         @"Redmond",@"address[city]",
+                                         @"WA",@"address[state]",
+                                         @"98052",@"address[zip]",
+                                         @"US",@"address[country]",
+                                         @"(425)882-8080",@"address[phone]",
+                                         @"bill@microsoft.com",@"address[email]",
+                                         nil];
+    
+    NSMutableDictionary *parcelDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                             @"8",@"parcel[length]",
+                                             @"6",@"parcel[width]",
+                                             @"5",@"parcel[height]",
+                                             @"10",@"parcel[weight]",
+                                             nil];
+```
+ 
+2. Use convenience methods to get data for postage
+--------------------
+```
+
+[EasyPost getShipmentTo:toDictionary from:fromDictionary forParcel:parcelDictionary
+      withCompletionHandler:^(NSError *error, NSDictionary *result) {
+          NSLog(@"Result: %@",result);
+          //Get rateId here
+          NSArray *rates = [result valueForKey:@"rates"];
+          NSString *shipmentId = [result valueForKey:@"id"];
+          if (rates.count > 0) {
+              
+              NSDictionary *firstRate = [rates objectAtIndex:0];
+              NSString *firstRateId = [firstRate valueForKey:@"id"];
+              
+              [EasyPost getPostageLabelForShipment:shipmentId atRate:firstRateId withCompletionHandler:^(NSError *error, NSDictionary *result) {
+                  [me.activity stopAnimating];
+                  me.generatingView.hidden = YES;
+                  if (error) {
+                      //Show some error
+                  } else {
+                      NSDictionary *postageDict = [result objectForKey:@"postage_label"];
+                      NSString *postageURL = [postageDict valueForKey:@"label_url"];
+                      NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:postageURL]];
+                      NSError *error = nil;
+                      //Data returned is a txt file
+                      NSHTTPURLResponse* response = nil;
+                    
+                      //Using synchronous request for convenience
+                      NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+                      
+                      if (returnData && !error) {
+                          UIImage *image = [UIImage imageWithData:returnData];
+                      } else {
+                        NSLog(@"Error downloading postage = %@", error);
+                      }
+                  }
+              }];
+          } else {
+              NSLog(@"No rates found!");
+          }
+      }];
+      
+```      
+
+3. Get image of postage!
+--------------------
+![QBLog](http://www.topbalancesoftware.com/apps/gitmedia/easypost_sample.png =500x750)
+
+
+
+MIT License
+--------------------
+    The MIT License (MIT)
+
+    Copyright (c) 2014 Ruben Nieves
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
